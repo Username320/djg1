4cb1c3eb2a76cab14a5efc0df5ae2d90afd6340b
name <name@n>
2024-12-14 17:38:20 +0300

    03

--- first_app/views.py
+++ first_app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from datetime import datetime
+def index_page(request):
+    context = {
+        "name": "00"
+    }
 
-# Create your views here.
+    return render(request, "index.html", context)
+def time_page(request):
+    now = datetime.now()
+    date_time_str = now.strftime("%Y-%m-%d %H:%M:%S")
+    context = {
+        "date": date_time_str[0:10],
+        "time": date_time_str[11::]
+    }
+
+    return render(request, "time.html", context)
